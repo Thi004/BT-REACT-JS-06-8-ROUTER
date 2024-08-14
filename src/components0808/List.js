155e4c69aa3a89d27554a0fb75a1fb2162e72a0e
Thi004 <phamthuymanthi@gmail.com>
2024-08-14 10:18:40 +0700

    them row

--- src/components0808/List.js
+++ src/components0808/List.js
@@ -1,29 +1,30 @@
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import axios from 'axios';
+
 export default function List() {
     const [list, setList] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:3000/students').then(data => {
-        setList(data.data)
-    })
-    },[])
+            setList(data.data)
+        })
+    }, [])
     return (
         <>
             <div className="row">
                 <div className="col-12"><h1>List Student</h1>
                     {list.map(item => (
-                        <>
-                            <div className="col-4">
+                        <div className="row">
+                            <div className="col-4 mt-2">
                                 <div className="card">
                                     <div className="card-body">
                                         <h5 className="card-title">{item.name}</h5>
                                         <p className="card-text">{item.score}</p>
-                                        <Link to={'/detail'+item.id}>Go Detail Student</Link>
+                                        <Link to={'/detail' + item.id}>Go Detail Student</Link>
                                     </div>
                                 </div>
                             </div>
-                        </>
+                        </div>
                     ))}
                 </div>
             </div>
